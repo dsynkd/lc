2bdac4f10c25a03a88590dc006413dceb7d33a38
dsynkd <darrensadr@gmail.com>
2020-10-12 21:28:42 -0500

    short solution for 322

--- 322.py
+++ 322.py
@@ -1,11 +1,7 @@
-from typing import List
 def coinChangeDP(coins, rem, count: List[int]):
-	if rem < 0: return -1
-	if rem == 0: return 0
-	if count[rem-1] != 0: return count[rem-1]
-	min = sys.maxsize
-	for v in coins:
-		res = coinChangeDP(coins, rem-v, count)
-		if res >= 0 and res < min: min = res+1
-	count[rem-1] = -1 if min == sys.maxsize else min
-	return count[rem-1]
+    dp = [0] + [float('inf') for _ in range(s)]
+    for v in c:
+        for i in range(v,s+1):
+            dp[i] = min(dp[i], dp[i-v]+1)
+    if dp[s] == float('inf'): return -1
+    return dp[s]
